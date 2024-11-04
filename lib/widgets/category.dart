49b94979c6b3e4c36027bba5bafe84e8e6e56a6e
LeeJueon <loolloo080313@gmail.com>
2024-11-04 22:34:17 +0900

    플러터 유튜브 클론 코딩

--- lib/widgets/category.dart
+++ lib/widgets/category.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class Category extends StatelessWidget {
-  const Category({super.key});
+class Category extends StatefulWidget {
+  const Category({
+    super.key,
+    required this.icon,
+    required this.category,
+    required this.ispush,
+  });
+
+  final IconData icon;
+  final String category;
+  final bool ispush;
+
+  @override
+  State<Category> createState() => _CategoryState();
+}
+
+class _CategoryState extends State<Category> {
+  late bool ispush;
+
+  @override
+  void initState() {
+    super.initState();
+    ispush = widget.ispush;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListWheelScrollView(
-        itemExtent: itemExtent, children: children
+    return Container(
+      child: ElevatedButton.icon(
+        onPressed: () {
+          setState(() {
+            ispush = !ispush;
+          });
+        },
+        icon: Icon(widget.icon),
+        label: Text(widget.category),
+      ),
     );
   }
 }
