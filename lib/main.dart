a1eb5421c2c5e7fea534ca1133945c2606a40c33
LeeJueon <loolloo080313@gmail.com>
2024-11-05 23:04:40 +0900

    동영상 필터 AppBar에 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:youtubeclonecording/widgets/category_list.dart';
 import 'package:youtubeclonecording/widgets/myprofile.dart';
 import 'package:youtubeclonecording/widgets/top_button.dart';
 import 'package:youtubeclonecording/screens/sharing_screen.dart';
@@ -22,48 +23,44 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.white ,
         appBar: PreferredSize(
-          preferredSize: const Size(200, 50),
+          preferredSize: const Size(1000, 100),
           child: Padding(
             padding: const EdgeInsets.only(top: 50,left: 10,right: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
               children: [
                 Flexible(
-                    flex: 2,
-                    child: Image.asset('assets/images/youtubelogo.png')
-                ),
-                const Flexible(
-                    flex: 1,
-                    child: SizedBox(),
-                ),
-                const TopButton(icon: Icons.screen_share,widget: SharingScreen()),
-                const TopButton(icon: CupertinoIcons.alarm,widget: AlarmScreen()),
-                const TopButton(icon: CupertinoIcons.search,widget: SearchScreen(),),
-                const Flexible(
                   flex: 1,
-                  child: Myprofile(size: 32,),
-                )
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                          flex: 2,
+                          child: Image.asset('assets/images/youtubelogo.png')
+                      ),
+                      const Flexible(
+                          flex: 1,
+                          child: SizedBox(),
+                      ),
+                      const TopButton(icon: Icons.screen_share,widget: SharingScreen()),
+                      const TopButton(icon: CupertinoIcons.alarm,widget: AlarmScreen()),
+                      const TopButton(icon: CupertinoIcons.search,widget: SearchScreen(),),
+                      const Flexible(
+                        flex: 1,
+                        child: Myprofile(size: 32,),
+                      )
+                    ],
+                  ),
+                ),
+                Flexible(
+                    flex: 1,
+                    child: CategoryList()
+                ),
               ],
             ),
           ),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-          ListView(
-          scrollDirection: Axis.horizontal,
-          children: [
-            Category_Button(category: "전체", ispush: ispush),
-            Category_Button(category: "게임", ispush: ispush),
-            Category_Button(category: "음악", ispush: ispush),
-            Category_Button(category: "라이브", ispush: ispush),
-            Category_Button(category: "뉴스", ispush: ispush),
-            Category_Button(category: "요리", ispush: ispush),
-            Category_Button(category: "최근에 업로드된 영상", ispush: ispush),
-            Category_Button(category: "감상한 동영상", ispush: ispush),
-            Category_Button(category: "새로운 맞춤 동영상", ispush: ispush)
-              ],
-            )
           ],
         ),
       ),
