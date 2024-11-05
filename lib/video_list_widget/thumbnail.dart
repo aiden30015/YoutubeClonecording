531a67719e80abe2f0d86273323b246d097074b2
LeeJueon <loolloo080313@gmail.com>
2024-11-05 23:39:06 +0900

    동영상 썸내일

---
+++ lib/video_list_widget/thumbnail.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Thumbnail extends StatelessWidget {
+  const Thumbnail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
