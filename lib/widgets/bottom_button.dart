291773d438569d43a7948db6d0f700a392ba51f3
LeeJueon <loolloo080313@gmail.com>
2024-11-11 20:41:04 +0900

    하단 버튼 추가

---
+++ lib/widgets/bottom_button.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class BottomButton extends StatelessWidget {
+  const BottomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ],
+    );
+  }
+}
