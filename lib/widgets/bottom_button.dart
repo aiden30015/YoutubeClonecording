0a73c90c6216befc5bbe089d11472bd0cfe1d545
LeeJueon <loolloo080313@gmail.com>
2024-11-11 20:57:55 +0900

    대충 하단 버튼들

--- lib/widgets/bottom_button.dart
+++ lib/widgets/bottom_button.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class BottomButton extends StatelessWidget {
-  const BottomButton({super.key});
+  const BottomButton({super.key, required this.icon, required this.text, required this.push});
+
+  final IconData icon;
+  final String text;
+  final Function push;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        
+        Expanded(
+          flex: 2,
+          child: IconButton(
+            onPressed: () => push,
+            icon: Icon(icon),
+          ),
+        ),
+        Expanded(
+            flex: 1,
+            child: Text(
+              text
+            )
+        ),
       ],
     );
   }
