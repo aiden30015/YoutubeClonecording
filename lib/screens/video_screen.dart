c6a00ec71d57d1f69f03217e6e6dbd3b88cdd855
LeeJueon <loolloo080313@gmail.com>
2024-11-16 12:17:39 +0900

    뒤로가기 버튼 추가

--- lib/screens/video_screen.dart
+++ lib/screens/video_screen.dart
@@ -1,24 +1,51 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class VideoScreen extends StatelessWidget {
-  const VideoScreen({super.key, required this.videoId});
+class VideoScreen extends StatefulWidget {
   final String videoId;
+
+  const VideoScreen({super.key, required this.videoId});
+
+  @override
+  State<VideoScreen> createState() => _VideoScreenState();
+}
+
+class _VideoScreenState extends State<VideoScreen> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      initialVideoId: widget.videoId,
+      flags: const YoutubePlayerFlags(
+        autoPlay: true,
+        mute: false,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final YoutubePlayerController _controller = YoutubePlayerController(initialVideoId: videoId);
-
-    return MaterialApp(
-      home: Scaffold(
-        body: YoutubePlayer(
-            controller: _controller,
-          showVideoProgressIndicator: true,
-          onReady: (){
-              debugPrint("Player is ready.");
-          }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Video Player"),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
+      ),
+      body: YoutubePlayer(
+        controller: _controller,
+        showVideoProgressIndicator: true,
       ),
     );
   }
